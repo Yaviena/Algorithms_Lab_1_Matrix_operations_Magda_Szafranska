23c214f84e4bacf61c6e8a4a67f414c25f67d79d
Magda Szafranska <magdalena.szafranska@outlook.com>
2022-01-04 00:22:55 +0100

    Initial commit

---
+++ Magda_Szafranska_18345_WSH_algorytmy_lab/Magda_Szafranska_18345_WSH_algorytmy_lab.cpp
@@ -0,0 +1,12 @@
+// Autor: Magda Szafrañska, nr indeksu WSH: 18345
+// Informatyka NST, rok 2, sem. 3
+// Algorytmy, laboratoria nr 1
+
+using namespace std;
+
+#include <iostream>
+
+int main()
+{
+    cout << "Hello World!\n";
+}
